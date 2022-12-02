9150d5a1d4251122988223d3a99a8622f64bd4e5
Ken <gialap2002@gmail.com>
2022-12-02 19:09:00 +0700

    add post task logic

---
+++ js/controllers/createTaskForm.js
@@ -0,0 +1,90 @@
+const baseUrl = "https://localhost:7231/api";
+const form = document.getElementById("form");
+const formWrapper = document.getElementById("form-wrapper");
+const formLabel = document.getElementById("label");
+const formDescription = document.getElementById("description");
+const formAssignee = document.getElementById("assignee");
+const formPriority = document.getElementById("priority");
+const formVehicle = document.getElementById("vehicle");
+const formMcp = document.getElementById("mcp");
+
+const openFormBtn = document.querySelector(".create-btn");
+const modalBackdrop = document.getElementById("modal-backdrop");
+const taskNo = document.getElementById("task-no");
+
+const getDefaultSelectOption = (opt) => {
+	return `<option value="" disabled selected>Choose ${opt}</option>`;
+};
+
+form.addEventListener("submit", async (e) => {
+	e.preventDefault();
+
+
+	const taskData = {
+		label: formLabel.value,
+		description: formDescription.value,
+		priority: formPriority.value,
+		userId: formAssignee.value,
+		vehicleId: formVehicle.value,
+		mcpId: formMcp.value,
+		created_at: moment(new Date()).format("DD/MM/YYYY")
+	};
+
+	const res = await axios.post(`${baseUrl}/tasks`, taskData);
+	const newTasksData = res.data.data;
+	const curData = JSON.parse(sessionStorage.getItem("DATA"));
+
+	curData.tasks = newTasksData;
+	sessionStorage.setItem('DATA', JSON.stringify(curData));
+
+	location.reload();
+});
+
+openFormBtn.addEventListener("click", (e) => {
+	const data = JSON.parse(sessionStorage.getItem("DATA"));
+
+	const taskId = data.tasks.length + 1;
+	taskNo.innerHTML = taskId >= 10 ? taskId : `0${taskId}`;
+
+	const userOptions = data.users.map((user) => {
+		return {
+			fullname: user.fullname,
+			id: user.id,
+		};
+	});
+	const mcpOptions = data.mcps.map((mcp) => {
+		return {
+			id: mcp.id,
+			address: mcp.address,
+		};
+	});
+	const vehicleOptions = data.vehicle.map((vehicle) => vehicle.id);
+
+	formVehicle.innerHTML = getDefaultSelectOption("vehicle");
+	formMcp.innerHTML = getDefaultSelectOption("MCP");
+	formAssignee.innerHTML = getDefaultSelectOption("Assignee");
+
+	userOptions.forEach((opt) => {
+		const option = document.createElement("option");
+		option.value = opt.id;
+		option.text = opt.fullname;
+
+		formAssignee.appendChild(option);
+	});
+
+	mcpOptions.map((opt) => {
+		const option = document.createElement("option");
+		option.value = opt.id;
+		option.text = opt.id + "    -    " + opt.address;
+
+		formMcp.appendChild(option);
+	});
+
+	vehicleOptions.map((opt) => {
+		const option = document.createElement("option");
+		option.value = opt;
+		option.text = opt;
+
+		formVehicle.appendChild(option);
+	});
+});
--- js/controllers/dashboard.js
+++ js/controllers/dashboard.js
@@ -1,16 +1,4 @@
-const form = document.getElementById("form");
-const formWrapper = document.getElementById("form-wrapper");
-const formLabel = document.getElementById("label");
-const formDescription = document.getElementById("description");
-const formAssignee = document.getElementById("assignee");
-const formPriority = document.getElementById("priority");
-const formVehicle = document.getElementById("vehicle");
-const formMcp = document.getElementById("mcp");
-
-const openFormBtn = document.querySelector(".create-btn");
 const baseUrl = "https://localhost:7231/api";
-const modalBackdrop = document.getElementById("modal-backdrop");
-
 const quantityContent = document.querySelectorAll(".c-content .quantity");
 const taskProgress = document.querySelector(".task-progress .front");
 const pieChart = document.getElementById("wastedProportion");
@@ -19,10 +7,6 @@ const barChart = document.getElementById("wastedLast5Days");
 const graphColors = ["#f43b41", "#31e36c", "#6b8beb", "#ef9834", "#9a580c"];
 
 let data = {};
-
-const getDefaultSelectOption = (opt) => {
-  return `<option value="" disabled selected>Choose ${opt}</option>`;
-};
 
 const getObjectOfData = (arr, key, value) => {
   const obj = {};
@@ -100,9 +84,12 @@ const getData = async () => {
 const displayData = () => {
   Object.entries(data).forEach(([key, value], idx) => {
     if (quantityContent[idx]) {
-      quantityContent[idx].innerHTML = `${value.length}${
-        key === "tasks" ? "%" : ""
-      }`;
+      if (key === "tasks") {
+        const donePercentage =
+          (value.filter((task) => task.isdone).length / value.length) * 100;
+        taskProgress.style.width = `${donePercentage}%`;
+        quantityContent[idx].innerHTML = `${Math.round(donePercentage)}%`;
+      } else quantityContent[idx].innerHTML = value.length;
     }
   });
 
@@ -156,62 +143,10 @@ const displayData = () => {
   );
 };
 
-if (sessionStorage.getItem("DATA") === null) {
+if (!sessionStorage.getItem("DATA")) {
   getData();
 } else {
   data = JSON.parse(sessionStorage.getItem("DATA"));
 }
 
 displayData();
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const taskData = {
-    label: formLabel.value,
-    description: formDescription.value,
-    assignee: formAssignee.value,
-    priority: formPriority.value,
-    vehicle: formVehicle.value,
-    mcp: formMcp.value,
-  };
-  console.log(taskData);
-});
-
-openFormBtn.addEventListener("click", (e) => {
-  const userOptions = data.users.map((user) => user.fullname);
-  const mcpOptions = data.mcps.map((mcp) => {
-    return {
-      id: mcp.id,
-      address: mcp.address,
-    };
-  });
-  const vehicleOptions = data.vehicle.map((vehicle) => vehicle.id);
-
-  userOptions.forEach((opt) => {
-    const option = document.createElement("option");
-    option.value = opt;
-    option.text = opt;
-
-    formAssignee.innerHTML = getDefaultSelectOption("Assignee");
-    formAssignee.appendChild(option);
-  });
-
-  mcpOptions.map((opt) => {
-    const option = document.createElement("option");
-    option.value = opt.id;
-    option.text = opt.id + "    -    " + opt.address;
-
-    formMcp.innerHTML = getDefaultSelectOption("MCP");
-    formMcp.appendChild(option);
-  });
-
-  vehicleOptions.map((opt) => {
-    const option = document.createElement("option");
-    option.value = opt;
-    option.text = opt;
-
-    formVehicle.innerHTML = getDefaultSelectOption("vehicle");
-    formVehicle.appendChild(option);
-  });
-});
